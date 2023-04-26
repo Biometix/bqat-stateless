a51985a6d5bb106393c8f08bc1428caaf74ca462
L.Wu <l.wu@biometix.com>
2023-04-26 16:48:31 +1000

    Update report links

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
-docker run --rm -it \
+docker run --rm \
     --shm-size=2G \
-    -v "$(pwd)"/reports:/app/reports \
-    -v "$(pwd)"/data:/app/data \
-    bqat-stateless \
-    "python3.8 -m pytest tests -v --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --self-contained-html && genbadge tests -o reports/junit/tests-badge.svg && touch config.py config-3.py && coverage run -m pytest tests -v && coverage xml -o reports/coverage/coverage.xml && coverage html -d reports/coverage/ && genbadge coverage -o reports/coverage/coverage-badge.svg"
+    ghcr.io/biometix/bqat-stateless \
+    "python3.8 -m pytest tests --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --self-contained-html && genbadge tests -o reports/junit/tests-badge.svg && touch config.py config-3.py && coverage run -m pytest tests && coverage xml -o reports/coverage/coverage.xml && coverage html -d reports/coverage/ && genbadge coverage -o reports/coverage/coverage-badge.svg"
